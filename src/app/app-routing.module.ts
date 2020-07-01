beb12c1a3474fd11682ee46722baa93b4a5a7183
Sean Langford <me@seanlangford.com>
2020-07-01 15:03:07 -0400

    first put

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageTwoComponent } from './page-two/page-two.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '2',
+    component: PageTwoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageTwoComponent
   ],
   imports: [
     BrowserModule,
